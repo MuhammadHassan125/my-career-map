8746130a464d8e5006567292488819736634084a
hr1938082 <hassan.raza00567@gmail.com>
2024-08-23 14:34:44 +0000

    server changes

--- src/Fire/useFire.ts
+++ src/Fire/useFire.ts
@@ -3,10 +3,10 @@ import { useState } from "react";
 import Fire from "./Fire";
 
 // local ip
-export const baseURL = "http://192.168.18.194:8001/api";
+// export const baseURL = "http://192.168.18.194:8001/api";
 
 // server ip
-// export const baseURL = "http://64.23.166.88:4000/api";
+export const baseURL = "http://64.23.166.88:4000/api";
 
 const useFire = <T extends Record<string, any> = {}>(initialState: T = {} as T, initialProcessing?: boolean) => {
     type StateKeyType = keyof typeof initialState;
